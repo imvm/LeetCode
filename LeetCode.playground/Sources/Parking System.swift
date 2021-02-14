aeff4c958d426f5fca57622120d338dfb237849d
Ian Manor <ianmanor.dev@gmail.com>
2021-02-14 18:28:23 -0300

    Add parking system

---
+++ LeetCode.playground/Sources/Parking System.swift
@@ -0,0 +1,35 @@
+class ParkingSystem {
+    var big: Int
+    var medium: Int
+    var small: Int
+
+    init(_ big: Int, _ medium: Int, _ small: Int) {
+        self.big = big
+        self.medium = medium
+        self.small = small
+    }
+
+    func addCar(_ carType: Int) -> Bool {
+        switch carType {
+        case 1:
+            if big > 0 {
+                big -= 1
+                return true
+            }
+        case 2:
+            if medium > 0 {
+                medium -= 1
+                return true
+            }
+        case 3:
+            if small > 0 {
+                small -= 1
+                return true
+            }
+        default:
+            fatalError("Car doesn't exist")
+        }
+
+        return false
+    }
+}
