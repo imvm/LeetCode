c9b7ee447668eb8eb11e261269234204f30427b6
Ian Manor <ianmanor.dev@gmail.com>
2021-02-17 19:24:13 -0300

    Update description

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -1,3 +1,19 @@
 extension Solution {
+    func smallerNumbersThanCurrent(_ nums: [Int]) -> [Int] {
+        let sortedNumbers = nums.sorted()
+        var lastIndex = 0
+        var dict: Dictionary<Int, Int> = [:]
 
+        for sortedNumIndex in 0..<sortedNumbers.count {
+            let currentNumber = sortedNumbers[sortedNumIndex]
+            let lastNumber = sortedNumbers[lastIndex]
+            dict[currentNumber] = lastIndex
+
+            if currentNumber != lastNumber {
+                lastIndex = sortedNumIndex
+            }
+        }
+
+        return nums.map({ dict[$0]! })
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LeetCode
+Repository of LeetCode solutions in Swift.
